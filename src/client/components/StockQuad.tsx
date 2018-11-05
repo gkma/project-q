c101eecdc839d3a7a8255336592fdfb65919ce73
Garron Ma <garronma@gmail.com>
2018-11-05 13:59:05 -0800

    added stockquad

---
+++ src/client/components/StockQuad.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const StockQuad = () => (
+  <div className="quad" id="stock-quad">
+    <div className="quad-box">
+      <h1>StockQuad</h1>
+      <p>Feature coming soon!</p>
+    </div>
+  </div>
+);
+
+export default StockQuad;
